1ba6867afe330238c283df4736ec9d5b266c6d61
amitvagner@gmail.com <amitvagner@gmail.com>
2020-03-04 10:56:35 +0200

    delete resume token on fatal error

--- utils.js
+++ utils.js
@@ -19,13 +19,13 @@ exports.sleep = sleep;
 const getObjectPropFromString = (obj, propString, delimiter = ".") => {
 	let result = obj;
 	const arr = propString.split(delimiter);
-	for(let i in arr){
+	for (let i in arr) {
 		if (result[arr[i]] === undefined) {
 			return undefined;
 		}
 		result = result[arr[i]];
 		
-	};
+	}
 	
 	return result;
 };
@@ -35,3 +35,4 @@ exports.getObjectPropFromString = getObjectPropFromString;
 
 exports.DUPLICATE_CODE_ERROR = 11000;
 exports.RESUME_TOKEN_ERROR = 40585;
+exports.CHANGE_STREAM_FATAL_ERROR = 280;
